e07ba7fdc1e74a07e434415c37cf1b1b9e3fc135
Viacheslav Varnavskyi <ipz224_vvo@student.ztu.edu.ua>
2023-04-21 01:37:36 +0300

    Додано контроллер для взаємодії з категоріями, та додано функціонал імплементації категорії

--- ToDoList/ToDoList/DAL/Implementations/CategoryDAL.cs
+++ ToDoList/ToDoList/DAL/Implementations/CategoryDAL.cs
@@ -20,5 +20,41 @@ namespace ToDoList.DAL.Implementations
                 return connection.QueryFirstOrDefault<Category>("SELECT * FROM [Categories] WHERE Id=@Id", new { Id = id });
             }
         }
+        public void AddCategory(Category category)
+        {
+            using (var connection = DBConnection.CreateConnection())
+            {
+                connection.Query<ToDoItem>("INSERT INTO [Categories] (Name, Description) " +
+                    "VALUES (@Name, @Description)",
+                    new
+                    {
+                        Name = category.Name,
+                        Description = category.Description
+                    });
+            }
+        }
+        public void EditCategory(Category category)
+        {
+            using (var connection = DBConnection.CreateConnection())
+            {
+                connection.Query<ToDoItem>("UPDATE [Categories] " +
+                    "SET Name = @Name, Description = @Description" +
+                    "Where Id = @Id",
+                    new
+                    {
+                        Name = category.Name,
+                        Description = category.Description,
+                        Id = category.Id
+                    });
+            }
+        }
+        public void DeleteCategory(int id)
+        {
+            using (var connection = DBConnection.CreateConnection())
+            {
+                var ent = connection.QueryFirstOrDefault<ToDoItem>("DELETE FROM [Categories] WHERE Id=@Id", new { Id = id });
+            }
+        }
+
     }
 }
--- ToDoList/ToDoList/DAL/Implementations/ToDoItemDAL.cs
+++ ToDoList/ToDoList/DAL/Implementations/ToDoItemDAL.cs
@@ -4,84 +4,82 @@ using ToDoList.DAL.Models;
 
 namespace ToDoList.DAL.Implementations
 {
-	public class ToDoItemDAL : IToDoItem
-	{
-		public static string DateTimeToString(DateTime? dateTime)
-		{
-			DateTime temp_date;
-			if (dateTime != null)
-			{
-				temp_date = Convert.ToDateTime(dateTime);
+    public class ToDoItemDAL : IToDoItem
+    {
+        public static string DateTimeToString(DateTime? dateTime)
+        {
+            DateTime temp_date;
+            if (dateTime != null)
+            {
+                temp_date = Convert.ToDateTime(dateTime);
 
-				return temp_date.ToString("yyyy-MM-dd HH:mm");
-			}
-			return null;
+                return temp_date.ToString("yyyy-MM-dd HH:mm");
+            }
+            return null;
+        }
+        public ToDoItem GetToDoItemById(int id)
+        {
+            using (var connection = DBConnection.CreateConnection())
+            {
+                var ent = connection.QueryFirstOrDefault<ToDoItem>("SELECT * FROM [Tasks] WHERE Id=@Id", new { Id = id });
+                ent.Category = connection.QueryFirstOrDefault<Category>("SELECT * FROM [Categories] WHERE Id=@cat_id", new { cat_id = ent.CategoryId });
+                return ent;
+            }
+        }
+        public List<ToDoItem> GetToDoItems()
+        {
+            using (var connection = DBConnection.CreateConnection())
+            {
+                var entyties = connection.Query<ToDoItem>("SELECT * FROM [Tasks]").ToList();
+                foreach (var item in entyties)
+                {
+                    item.Category = connection.QueryFirstOrDefault<Category>("SELECT * FROM [dbo].[Categories] WHERE Id=@cat_id", new { cat_id = item.CategoryId });
 
+                }
 
-		}
-		public ToDoItem GetToDoItemById(int id)
-		{
-			using (var connection = DBConnection.CreateConnection())
-			{
-				var ent = connection.QueryFirstOrDefault<ToDoItem>("SELECT * FROM [Tasks] WHERE Id=@Id", new { Id = id });
-				ent.Category = connection.QueryFirstOrDefault<Category>("SELECT * FROM [Categories] WHERE Id=@cat_id", new { cat_id = ent.CategoryId });
-				return ent;
-			}
-		}
-		public List<ToDoItem> GetToDoItems()
-		{
-			using (var connection = DBConnection.CreateConnection())
-			{
-				var entyties = connection.Query<ToDoItem>("SELECT * FROM [Tasks]").ToList();
-				foreach (var item in entyties)
-				{
-					item.Category = connection.QueryFirstOrDefault<Category>("SELECT * FROM [dbo].[Categories] WHERE Id=@cat_id", new { cat_id = item.CategoryId });
-
-				}
-
-				return entyties;
-			}
-		}
-		public void AddToDoItem(ToDoItem toDoItem)
-		{
-			using (var connection = DBConnection.CreateConnection())
-			{
-				connection.Query<ToDoItem>("INSERT INTO [Tasks] (Text, Due_date, CategoryId, Set_Date, Is_Finished) " +
-					"VALUES (@Text, @Due_Date, @CategoryId,@Set_Date,@Is_Finished)",
-					new
-					{
-						Text = toDoItem.Text,
-						Due_Date = toDoItem.Due_Date,
-						CategoryId = toDoItem.CategoryId,
-						Set_Date = toDoItem.Set_Date,
-						Is_Finished = toDoItem.Is_Finished
-					});
-			}
-		}
-		public void EditToDoItem(ToDoItem toDoItem)
-		{
-			using (var connection = DBConnection.CreateConnection())
-			{
-				connection.Query<ToDoItem>("UPDATE [Tasks] " +
-					"SET Text = @Text, Due_Date = @Due_Date, CategoryId = @CategoryId, Set_Date = @Set_Date, Is_Finished = @Is_Finished " +
-					"Where Id = @Id",
-					new
-					{
-						Text = toDoItem.Text,
-						Due_Date = toDoItem.Due_Date,
-						CategoryId = toDoItem.CategoryId,
-						Set_Date = toDoItem.Set_Date,
-						Is_Finished = toDoItem.Is_Finished,
-						Id = toDoItem.Id
-					});
-			}
-		}
-		public void DeleteToDoItem(int id)
-		{
-			using (var connection = DBConnection.CreateConnection())
-			{
-				var ent = connection.QueryFirstOrDefault<ToDoItem>("DELETE FROM [Tasks] WHERE Id=@Id", new { Id = id });
-			}
-		}
-	}
+                return entyties;
+            }
+        }
+        public void AddToDoItem(ToDoItem toDoItem)
+        {
+            using (var connection = DBConnection.CreateConnection())
+            {
+                connection.Query<ToDoItem>("INSERT INTO [Tasks] (Text, Due_date, CategoryId, Set_Date, Is_Finished) " +
+                    "VALUES (@Text, @Due_Date, @CategoryId,@Set_Date,@Is_Finished)",
+                    new
+                    {
+                        Text = toDoItem.Text,
+                        Due_Date = toDoItem.Due_Date,
+                        CategoryId = toDoItem.CategoryId,
+                        Set_Date = toDoItem.Set_Date,
+                        Is_Finished = toDoItem.Is_Finished
+                    });
+            }
+        }
+        public void EditToDoItem(ToDoItem toDoItem)
+        {
+            using (var connection = DBConnection.CreateConnection())
+            {
+                connection.Query<ToDoItem>("UPDATE [Tasks] " +
+                    "SET Text = @Text, Due_Date = @Due_Date, CategoryId = @CategoryId, Set_Date = @Set_Date, Is_Finished = @Is_Finished " +
+                    "Where Id = @Id",
+                    new
+                    {
+                        Text = toDoItem.Text,
+                        Due_Date = toDoItem.Due_Date,
+                        CategoryId = toDoItem.CategoryId,
+                        Set_Date = toDoItem.Set_Date,
+                        Is_Finished = toDoItem.Is_Finished,
+                        Id = toDoItem.Id
+                    });
+            }
+        }
+        public void DeleteToDoItem(int id)
+        {
+            using (var connection = DBConnection.CreateConnection())
+            {
+                var ent = connection.QueryFirstOrDefault<ToDoItem>("DELETE FROM [Tasks] WHERE Id=@Id", new { Id = id });
+            }
+        }
+    }
 }
